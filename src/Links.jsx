a2b45a378187a851a99f204b162b149dd89bb2ba
Ryan Gudsell <ryangudsell@gmail.com>
2023-11-21 09:24:21 +1300

    Started work on product page

--- src/Links.jsx
+++ src/Links.jsx
@@ -6,6 +6,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Services from './pages/Services'
 import SingleService from './pages/SingleService'
+import Product from './pages/Product'
 
 const Links = () => {
   return (
@@ -13,8 +14,9 @@ const Links = () => {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
+        <Route exact path="/services" element={<Services />} />
         <Route path="/services/:id" element={<SingleService />} />
+        <Route path="/services/:id/booking/:productId" element={<Product />} />
     </Routes>
   )
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,85 +8,37 @@ import Loading from '../components/Loading'
 import useCustomizer from '../hook/useCustomizer'
 
 const baseUrl = import.meta.env.VITE_WP_API_BASEURL
+const storeBaseUrl = import.meta.env.VITE_WC_PRODUCTS_URL
 
-const SingleService = () => {
-  const {id} = useParams()
+const Product = () => {
+  const {id, productId} = useParams()
   const navigate = useNavigate()
-  const endpoint = `${baseUrl}/services`
-  let testimonialEndpoint = ``
-
-  const [servicesArray, setServicesArray] = useState([])
-  const [servicesArrayIndex, setServicesArrayIndex] = useState(null)
+  const storeEndpoint = `${storeBaseUrl}/${productId}`
+  console.log(storeEndpoint);
 
   const [service, setService] = useState(null)
-  const [testimonial, setTestimonial] = useState(null)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios.get(`${endpoint}/${id}`)
+    console.log("axios call to", `${storeEndpoint}/${productId}`);
+    axios.get(`${storeEndpoint}`)
     .then((res) => {
       setService(res.data)
       setLoading(false)
+      console.log(service);
     })
     .catch((err) => console.log(err))
   }, [id])
 
-  useEffect(() => {
-    getTestimonial(service)
-  }, [service])
-
-  function getTestimonial (service) {
-    if (!service) return
-    testimonialEndpoint = `${baseUrl}/testimonials?service=${service.service[0]}`
-
-    axios.get(`${testimonialEndpoint}`)
-    .then((res) => {
-      if (res.data[0]) {
-        setTestimonial(res.data[0])
-      }
+  const getProduct = () => {
+    setStoreEndpoint(`${storeBaseUrl}/${Number(service.acf.quote_product_link)}`)
+    console.log(storeEndpoint);
+    axios.get(`${storeEndpoint}`)
+    .then((response) => {
+      setProduct(response.data)
+      console.log(response.data)
     })
-    .catch((err) => {console.log(err)})
-  }
-
-  // Prev/Next Buttons
-  useEffect(() => {
-    axios.get(`${endpoint}`)
-    .then((res) => {
-      setServicesArray(res.data.reverse())
-    })
-    .catch((err) => console.log(err))
-  }, [])
-
-  useEffect(() => {
-    let myService = service;
-    const foundIndex = servicesArray.findIndex((arrayItem) => arrayItem.id === myService.id);
-    setServicesArrayIndex(foundIndex);
-  }, [servicesArray, service]);
-
-  const prevService = () => {
-    let newIndex = servicesArrayIndex - 1
-    if (newIndex < 0) {
-      newIndex = servicesArray.length - 1
-    }
-    servicesArray.map((arrayItem, index) => {
-      if (newIndex === index) {
-        let newId = arrayItem.id
-        navigate(`/services/${newId}`)
-      }
-    })
-  }
-
-  const nextService = () => {
-    let newIndex = servicesArrayIndex + 1
-    if (newIndex > servicesArray.length - 2) {
-      newIndex = 0
-    }
-    servicesArray.map((arrayItem, index) => {
-      if (newIndex === index) {
-        let newId = arrayItem.id
-        navigate(`/services/${newId}`)
-      }
-    })
+    .catch((error) => console.log(error))
   }
 
   function decodeHTMLEntities(text) {
@@ -105,7 +57,7 @@ const SingleService = () => {
   return (<>
     <Helmet>
       {/* Primary Meta Tags */}
-      <title>What We Do | Peter's Auto Services</title>
+      <title>Book a Quote | Peter's Auto Services</title>
       <meta name="title" content="Peter's Auto Services" />
       <meta name="description" content="Peter's Auto Service in Titahi Bay, Porirua is focused on providing high-quality service and customer satisfaction, operating within the Wellington region since 2008." />
       <meta name="keywords" content="Peters Auto Services, Mechanic, Auto Shop, Porirua, "/>
@@ -130,7 +82,7 @@ const SingleService = () => {
       <section className="service landing">
         <div className='image-div'></div>
         <div className='text-div border-bottom'>
-          <h1>Services</h1>
+          <h1>Booking</h1>
         </div>
       </section>
       {loading ? <Loading /> : (<>
@@ -140,38 +92,15 @@ const SingleService = () => {
             onClick={() => {navigate(-1)}}><ArrowLeft /> Go Back</h3>
           <h2>{decodeHTMLEntities(service.title.rendered)}</h2>
           <div dangerouslySetInnerHTML={{ __html: service.content.rendered }} />
-          <button className='on-click'
-          onClick={() => {
-            navigate(`/services/${service.id}/booking/${service.acf.quote_product_link}`)
-          }}><h3>BOOK A QUOTE</h3></button>
+          <button className='on-click'><h3>BOOK A QUOTE</h3></button>
         </section>
         <section className='service service-image'>
           <img src={`${service.acf.service_image}`} alt='Service Image' />
         </section>
         </div>
-        {testimonial && 
-        <section className='service service-testimonials'>
-          <h2>Hear from Satisfied Customers</h2>
-          <div>
-            <p>“<span dangerouslySetInnerHTML={{__html: decodeHTMLEntities(testimonial.content.rendered)}} />”</p>
-
-            <p className='testimonial-author'> - {testimonial.acf.author}</p>
-          </div>
-        </section>}
-        <section className='service page-buttons'>
-          <div>
-            <h3 className='on-click prev-button' onClick={() => prevService()}>
-              <ArrowLeft /> <span>Previous Service</span>
-            </h3>
-
-            <h3 className='on-click next-button' onClick={() => nextService()}>
-              <span>Next Service</span> <ArrowRight />
-            </h3>
-          </div>
-        </section>
       </>)}
     </main>
   </>)
 }
 
-export default SingleService
+export default Product
